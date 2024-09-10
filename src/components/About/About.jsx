d7124b6deb610947ff61014645d844b27fbd2914
Padee Vang <vangpadee11@gmail.com>
2024-09-09 19:50:19 -0500

    completed

---
+++ src/components/About/About.jsx
@@ -0,0 +1,7 @@
+function About(){
+    return (
+        <>Welcome to the React Router Student List. Please add your Github username. </>
+    )
+}
+
+export default About;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,8 @@
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 import StudentForm from '../StudentForm/StudentForm.jsx';
 import StudentList from '../StudentList/StudentList.jsx';
-
+import About from "../About/About.jsx";
 
 function App() {
     return (
@@ -12,6 +13,29 @@ function App() {
             </header>
             <StudentForm />
             <StudentList />
+            
+            <nav>
+                <Router>
+                    <Link to="/">Main</Link> < br/>
+                    <Link to="/Home">Home</Link> < br/>
+                    <Link to="/About">About</Link> < br/>
+                    <Link to="/Students">Students</Link> < br/>
+                   
+                   
+                    <Route path="/Home">
+                        <StudentForm/>
+                    </Route>
+                   
+                    <Route path="/About">
+                        <About />
+                    </Route>
+                    
+                    <Route path="/Students">
+                        <StudentList />
+                    </Route>
+
+                </Router>
+            </nav>
         </div>
     );
 }
